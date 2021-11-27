79d5791126afd026a0e20b00eaf00d70b6ca986e
mcarajatchawla <mcarajatchawla@gmail.com>
2021-11-27 20:13:46 +0530

    junit changes

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,12 +1,16 @@
 package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AppTest 
 {
     /**
@@ -15,6 +19,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertTrue( false );
     }
 }
